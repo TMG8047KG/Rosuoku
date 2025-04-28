cad491e409df080a92824bbbc92c45be44be5fad
TMG <66220144+TMG8047KG@users.noreply.github.com>
2025-04-28 11:39:23 +0300

    inital commit

---
+++ src-tauri/src/grid.rs
@@ -0,0 +1,12 @@
+
+
+
+#[tauri::command]
+pub fn generate_sudoku() -> Vec<int> {
+    let grid = vec![[]];
+    for 0..9 in grid {
+        for 0..9 in grid {
+            
+        }
+    }
+}
---
+++ src/Board.tsx
@@ -0,0 +1,10 @@
+
+function board(){
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default board;
